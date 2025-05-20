894dc032dee257cb6e8340b12f52a2dbb5b2e78b
jvlyndark <50807142+jvlyndark@users.noreply.github.com>
2025-05-20 07:40:03 -0400

    Update DB to use PostgreSQL

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ import os
 
 
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY", "defaultsecret")
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:////app/obsedo.db")
+    SECRET_KEY = os.getenv("SECRET_KEY", "defaultsecret")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:////app/obsedo.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
